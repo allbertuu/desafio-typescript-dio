2200fddd6ab0dd5a13136e44a835a4e31f78b9c8
Alberto <albertovinicius3@gmail.com>
2022-06-22 10:55:43 -0300

    add js compiled

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 // O código abaixo tem alguns erros e não funciona como deveria.
 // Você pode identificar quais são e corrigi-los em um arquivo TS?
+// FINALIZADO - TUDO CERTO
 var botaoAtualizar = document.getElementById("atualizar-saldo");
 var botaoLimpar = document.getElementById("limpar-saldo");
 var soma = document.getElementById("soma");
 var campoSaldo = document.getElementById("campo-saldo");
 // inicializar campos em zero
-if (campoSaldo)
-    campoSaldo.innerHTML = "0";
-if (soma)
-    soma.value = "0";
+resetarCampos();
 /********************
-****** FUNÇÕES ******
-********************/
-var somarAoSaldo = function (soma) {
-    // previne uso do campo saldo se for um valor falsy
+ ****** FUNÇÕES *****
+ ********************/
+function resetarCampos() {
+    if (campoSaldo)
+        campoSaldo.innerHTML = "0";
+    if (soma)
+        soma.value = "0";
+}
+function somarAoSaldo(soma) {
+    // previne uso do campo saldo se o mesmo for um valor falsy no contexto booleano
     if (!campoSaldo) {
         return;
     }
@@ -24,17 +28,11 @@ var somarAoSaldo = function (soma) {
     campoSaldoConvertido += somaConvertida;
     // atualizar o campo saldo com o novo valor obtido na soma
     campoSaldo.innerHTML = campoSaldoConvertido.toString();
-};
-var limparSaldo = function () {
-    // resetar campos à zero
-    if (campoSaldo)
-        campoSaldo.innerHTML = "0";
-    if (soma)
-        soma.value = "0";
-};
+}
+var limparSaldo = function () { return resetarCampos(); };
 /********************
-** EVENT LISTENERS **
-********************/
+ ** EVENT LISTENERS *
+ ********************/
 if (botaoAtualizar)
     botaoAtualizar.addEventListener("click", function () {
         if (soma)
